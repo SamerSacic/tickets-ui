39123a041b0c2192ca0d8062950ef66cd44af156
SamerSacic <samer.sacic@gmail.com>
2021-12-01 01:16:28 +0100

    Remove shadow from tabs content

--- src/components/Event/Tab/Tabs.js
+++ src/components/Event/Tab/Tabs.js
@@ -127,7 +127,7 @@ const Tabs = ({ event, onEditEvent }) => {
               </a>
             </li>
           </ul>
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full my-6 shadow-lg rounded">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full my-6">
             <div className="py-5 flex-auto">
               <div className="tab-content tab-space">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
